633b7a72d0c5ea4820cf422fc8a4b579612aee12
Willem Victor <wimpievictor@gmail.com>
2022-09-07 18:30:01 +0200

    Parametrized test

--- tests/test_template.py
+++ tests/test_template.py
@@ -19,11 +19,15 @@ def test_url_lookup_invalid_input(base_url, exception):
         web_maker.template.create_url_lookup(base_url)
 
 
-def test_url_lookup():
+@pytest.mark.parametrize(
+    "base_url,file_path,url",
+    [
+        ("http://github.com", "content/index.md", "http://github.com/index.html"),
+    ],
+)
+def test_url_lookup(base_url, file_path, url):
     dir_paths = ["content/"]
     ext_map = {"md": "html"}
-    url_lookup = web_maker.template.create_url_lookup(
-        "http://github.com", dir_paths, ext_map
-    )
-    href = url_lookup("content/index.md")
-    assert href == "http://github.com/index.html"
+    url_lookup = web_maker.template.create_url_lookup(base_url, dir_paths, ext_map)
+    href = url_lookup(file_path)
+    assert href == url
